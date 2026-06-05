f7f557d235b98a748082850ef46b23e34b2f3384
Richard Higgins <rshiggin@users.noreply.github.com>
2026-06-05 13:26:28 -0400

    arclight native install

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  # Adds a few additional behaviors into the application controller
+  include Blacklight::Controller
+  include Blacklight::LocalePicker::Concern
+  layout :determine_layout if respond_to? :layout
+
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
+end
