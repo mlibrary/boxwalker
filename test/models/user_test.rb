518e38004dd911f0e6836c3707f75017d78b9a38
Greg Kostin <gkostin@umich.edu>
2026-06-08 14:36:16 -0400

    simple user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require_relative "../test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "truth" do
+    assert true
+  end
 end
