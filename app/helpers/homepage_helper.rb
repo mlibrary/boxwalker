6a1043548458a265d9127eab5339beb86923f9b0
Samuel Sciolla <ssciolla@umich.edu>
2026-06-15 10:37:18 -0400

    Start wiring up homepage by switching root to catalog#index, adding overriding templates for index, home, adding HomepageHelper and CatalogHelper, adding image config and files, adding um_arclight translation file

---
+++ app/helpers/homepage_helper.rb
@@ -0,0 +1,35 @@
+module HomepageHelper
+  def config_features
+    @config_features ||= begin
+                           YAML.safe_load(::File.read(config_filename))
+                         rescue Errno::ENOENT
+                           {}
+                         end
+  end
+
+  def config_filename
+    Rails.root.join('config', 'featured_images.yml')
+  end
+
+  def random_feature
+    img_index = config_features['image_list'].keys.sample
+    config_features['image_list'][img_index]
+  end
+
+  # Image can be an absolute URL to any external image, or alternatively,
+  # a name of a local file within assets/images/homepage
+  def feature_img_url(image)
+    return image if image.match(/^#{URI.regexp(%w[http https])}$/)
+
+    image_url(['homepage', image].join('/'))
+  end
+
+  def collection_count_home
+    search_service = Blacklight.repository_class.new(blacklight_config)
+    query = search_service.search(
+      q: 'level_sim:Collection',
+      rows: 1
+    )
+    query.response['numFound']
+  end
+end
