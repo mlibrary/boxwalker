bf243868228334f44d8b9921a97666aadfd8b34f
Samuel Sciolla <ssciolla@umich.edu>
2026-06-22 14:57:51 -0400

    Please rubocop

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -8,12 +8,12 @@ module HomepageHelper
   end
 
   def config_filename
-    Rails.root.join('config', 'featured_images.yml')
+    Rails.root.join("config", "featured_images.yml")
   end
 
   def random_feature
-    img_index = config_features['image_list'].keys.sample
-    config_features['image_list'][img_index]
+    img_index = config_features["image_list"].keys.sample
+    config_features["image_list"][img_index]
   end
 
   # Image can be an absolute URL to any external image, or alternatively,
@@ -21,15 +21,15 @@ module HomepageHelper
   def feature_img_url(image)
     return image if image.match(/^#{URI.regexp(%w[http https])}$/)
 
-    image_url(['homepage', image].join('/'))
+    image_url([ "homepage", image ].join("/"))
   end
 
   def collection_count_home
     search_service = Blacklight.repository_class.new(blacklight_config)
     query = search_service.search(
-      q: 'level_ssim:Collection',
+      q: "level_ssim:Collection",
       rows: 1
     )
-    query.response['numFound']
+    query.response["numFound"]
   end
 end
