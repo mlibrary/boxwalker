c04cb0bb8f6256ac6f2174323ed1cdaf80f07f14
Richard Higgins <rshiggin@users.noreply.github.com>
2026-06-25 14:44:31 -0400

    Remove access facet to match umich-arclight

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -175,10 +175,6 @@ class CatalogController < ApplicationController
     config.add_index_field "abstract_or_scope", accessor: true, truncate: true, repository_context: true, helper_method: :render_html_tags, component: Arclight::IndexMetadataFieldComponent
     config.add_index_field "breadcrumbs", accessor: :itself, component: Arclight::SearchResultBreadcrumbsComponent, compact: { count: 2 }
 
-    config.add_facet_field "access", query: {
-      online: { label: "Online access", fq: "has_online_content_ssim:true" }
-    }
-
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
 
