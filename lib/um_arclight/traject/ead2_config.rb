626e19235e28a2ca27dd9fb13c1df339d25a9d61
Samuel Sciolla <ssciolla@umich.edu>
2026-06-25 14:39:13 -0400

    Add commment; use double-quoted strings

--- lib/um_arclight/traject/ead2_config.rb
+++ lib/um_arclight/traject/ead2_config.rb
@@ -14,16 +14,17 @@ load_config_file Arclight::Engine.root.join("lib/arclight/traject/ead2_config.rb
 # Boxrunner-specific additions / overrides
 # ==========================================
 
-to_field 'access_subjects_ssim', extract_xpath('/ead/archdesc/controlaccess', to_text: false) do |_record, accumulator|
+# Utilize genreform for Formats facet
+
+to_field "access_subjects_ssim", extract_xpath("/ead/archdesc/controlaccess", to_text: false) do |_record, accumulator|
   accumulator.map! do |element|
-    # DUL CUSTOMIZATION: pull out genreform into its own field
     %w[subject function occupation].map do |selector|
       element.xpath(".//#{selector}").map(&:text)
     end
   end.flatten!
 end
 
-to_field 'formats_ssim', extract_xpath('/ead/archdesc/controlaccess/genreform|/ead/archdesc/controlaccess/controlaccess/genreform')
-to_field 'formats_ssm' do |_record, accumulator, context|
-  accumulator.concat Array.wrap(context.output_hash['formats_ssim'])
+to_field "formats_ssim", extract_xpath("/ead/archdesc/controlaccess/genreform|/ead/archdesc/controlaccess/controlaccess/genreform")
+to_field "formats_ssm" do |_record, accumulator, context|
+  accumulator.concat Array.wrap(context.output_hash["formats_ssim"])
 end
