c7d41e3087795e0cef79dba9c46cef9b0a454d54
Richard Higgins <rshiggin@users.noreply.github.com>
2026-07-06 18:04:08 -0400

    Remove the index-document-functions CSS class from actions method

--- app/components/um_search_result_title_component.rb
+++ app/components/um_search_result_title_component.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 class UmSearchResultTitleComponent < Arclight::SearchResultTitleComponent
+  def actions
+    return [] unless @actions
+
+    if block_given?
+      @has_actions_slot = true
+      return super
+    end
+
+    (@has_actions_slot && get_slot(:actions)) ||
+      ([@document_component&.actions] if @document_component&.actions.present?) ||
+      [helpers.render_index_doc_actions(presenter.document, wrapping_class: nil)]
+  end
 end
