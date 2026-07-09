38b58480ebb23b6a5c6ed5fbebdfecef6d2216db
Richard Higgins <rshiggin@users.noreply.github.com>
2026-07-09 17:58:46 -0400

    Rework to avoid disabling actions, with conditional for different scenarios

--- app/components/um_search_result_title_component.rb
+++ app/components/um_search_result_title_component.rb
@@ -1,17 +1,27 @@
 # frozen_string_literal: true
 
 class UmSearchResultTitleComponent < Arclight::SearchResultTitleComponent
+  def title_col_class
+    if presenter.document.containers.present? && actions.present?
+      "col-md-8"
+    elsif presenter.document.containers.present? && actions.blank?
+      "col-md-10"
+    elsif presenter.document.containers.blank? && actions.present?
+      "col-md-11"
+    else
+      "col"
+    end
+  end
   def actions
     return [] unless @actions
-
     if block_given?
       @has_actions_slot = true
-      return super
+      return
     end
 
-    # Keep explicit slot/document_component actions, but skip Blacklight fallback
-    (@has_actions_slot && get_slot(:actions)) ||
-      ([ @document_component&.actions ] if @document_component&.actions.present?) ||
-      []
+  (@has_actions_slot && get_slot(:actions)) ||
+    ([ @document_component&.actions] if @document_component&.actions.present?) ||
+    [ helpers.render_index_doc_actions(presenter.document, wrapping_class: "actions-wrapping-class col-md-1") ]
   end
 end
+
