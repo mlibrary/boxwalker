5e780d2a78f6b2589948d612cd9a5032de05f493
Samuel Sciolla <ssciolla@umich.edu>
2026-07-10 13:36:36 -0400

    Add missed lib/download_utility.rb

---
+++ lib/download_utility.rb
@@ -0,0 +1,53 @@
+class DownloadUtility
+  attr_accessor :document
+
+  def initialize(document)
+    @document = document
+  end
+
+  def ead_available?
+    File.exist?(ead_file_path)
+  end
+
+  def finding_aid_data
+    ENV.fetch("FINDING_AID_DATA", "data")
+  end
+
+  def ead_file_path
+    File.join(finding_aid_data, "xml", repo_slug, "#{ead_slug}.xml")
+  end
+
+  def html_available?
+    File.exist?(html_file_path)
+  end
+
+  def html_file_path
+    File.join(finding_aid_data, "pdf", repo_slug, "#{ead_slug}.html")
+  end
+
+  def pdf_available?
+    File.exist?(pdf_file_path)
+  end
+
+  def pdf_file_path
+    File.join(finding_aid_data, "pdf", repo_slug, "#{ead_slug}.pdf")
+  end
+
+  def xml_available?
+    ead_available?
+  end
+
+  def xml_file_path
+    ead_file_path
+  end
+
+  private
+
+  def repo_slug
+    document&.repository_config&.slug || "repo_slug"
+  end
+
+  def ead_slug
+    document&.document_id || "ead_slug"
+  end
+end
