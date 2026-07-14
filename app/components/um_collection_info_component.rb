323b4762ea1eb617f4ed551c91f4cb32000d8fbe
Richard Higgins <rshiggin@users.noreply.github.com>
2026-07-14 13:02:06 -0400

    Conform names to prod sidebar collection_info dropdown navigation, with minor style changes

--- app/components/um_collection_info_component.rb
+++ app/components/um_collection_info_component.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class UmCollectionInfoComponent < Arclight::CollectionInfoComponent
+
+  delegate :total_component_count, :online_item_count, :last_indexed, :collection_unitid, to: :collection
+  delegate :blacklight_icon, to: :helpers
+
   def info_icon
     content_tag(:i, "", class: "bi bi-info-circle-fill")
   end
