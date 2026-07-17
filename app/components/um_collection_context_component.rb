bbc6882564522204188893f33d7eeebcf96d37b4
Richard Higgins <rshiggin@users.noreply.github.com>
2026-07-17 15:26:34 -0400

    Clean up a few files

--- app/components/um_collection_context_component.rb
+++ app/components/um_collection_context_component.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UmCollectionContextComponent < Arclight::CollectionContextComponent
   def collection_info
     render UmCollectionInfoComponent.new(collection: collection)
--- app/components/um_document_collection_hierarchy_component.rb
+++ app/components/um_document_collection_hierarchy_component.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 class UmDocumentCollectionHierarchyComponent < Arclight::DocumentCollectionHierarchyComponent
 end
