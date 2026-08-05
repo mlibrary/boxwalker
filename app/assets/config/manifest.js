33bffddb59fbbaeacf061af39f9b2c3c9d8c4ded
Richard Higgins <rshiggin@users.noreply.github.com>
2026-08-05 13:19:53 -0400

    Add and revise 'manifest.js' for compiling with Sprockets 4.x

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
-//= link_tree ../builds
+//= link bootstrap.css
+//= link application.css
 //= link_tree ../../javascript .js
+//= link blacklight-range-limit/index.js
+//= link blacklight/manifest.js
