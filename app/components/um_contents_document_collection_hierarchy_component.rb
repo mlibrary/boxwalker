0253355e2790cad0785e727b900ee39da8979640
Samuel Sciolla <ssciolla@umich.edu>
2026-08-06 17:54:08 -0400

    Add work-in-progress on displaying contents hierarchy on the collection page

---
+++ app/components/um_contents_document_collection_hierarchy_component.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class UmContentsDocumentCollectionHierarchyComponent < Arclight::DocumentCollectionHierarchyComponent
+  def abstract_or_scope
+    field = if document.abstract
+      "abstract_html_tesm"
+    elsif document.scope
+      "scopecontent_html_tesm"
+    else
+      nil
+    end
+    return unless field
+
+    render Arclight::IndexMetadataFieldComponent.new(
+      field: Blacklight::FieldPresenter.new(
+        view_context,
+        document,
+        Blacklight::Configuration::Field.new(
+          field: field, key: "abstract_or_scope", truncate: true, helper_method: :render_html_tags
+        )
+      )
+    ) if document.abstract_or_scope
+  end
+end
