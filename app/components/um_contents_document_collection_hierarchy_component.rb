27138ea1871dabb14d4ece9749b37c7b8836c397
Samuel Sciolla <ssciolla@umich.edu>
2026-08-12 11:23:41 -0400

    Simplify abstract_or_scope method in contents component using accessor true

--- app/components/um_contents_document_collection_hierarchy_component.rb
+++ app/components/um_contents_document_collection_hierarchy_component.rb
@@ -2,21 +2,16 @@
 
 class UmContentsDocumentCollectionHierarchyComponent < Arclight::DocumentCollectionHierarchyComponent
   def abstract_or_scope
-    field = if document.abstract
-      "abstract_html_tesm"
-    elsif document.scope
-      "scopecontent_html_tesm"
-    else
-      nil
-    end
-    return unless field
-
     render Arclight::IndexMetadataFieldComponent.new(
       field: Blacklight::FieldPresenter.new(
         view_context,
         document,
         Blacklight::Configuration::Field.new(
-          field: field, key: "abstract_or_scope", truncate: true, helper_method: :render_html_tags
+          field: "abstract_or_scope",
+          key: "abstract_or_scope",
+          accessor: true,
+          truncate: true,
+          helper_method: :render_html_tags
         )
       )
     )
