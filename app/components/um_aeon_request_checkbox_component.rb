fc4b3aba03982fb8cb0b9d317e30b3ad3f97106a
Richard Higgins <rshiggin@users.noreply.github.com>
2026-08-28 12:34:51 -0400

    Select item button in leaf display toolbar, alongside Request and Clear selected

--- app/components/um_aeon_request_checkbox_component.rb
+++ app/components/um_aeon_request_checkbox_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class UmAeonRequestCheckboxComponent < ViewComponent::Base
-  def initialize(document:)
+  def initialize(document:, label: "Request")
     @document = document
+    @label = label
   end
 
-  attr_reader :document
+  attr_reader :document, :label
 end
