41c2500ce1891c80e4320987009761be8679d292
Richard Higgins <rshiggin@users.noreply.github.com>
2026-09-02 14:06:37 -0400

    Remove unintended visible label from checkboxes, minor style changes

--- app/components/um_aeon_request_checkbox_component.rb
+++ app/components/um_aeon_request_checkbox_component.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class UmAeonRequestCheckboxComponent < ViewComponent::Base
-  def initialize(document:, label: "Request")
+  def initialize(document:, label: nil)
     @document = document
     @label = label
   end
-
   attr_reader :document, :label
 end
